045c34f4820a40150c63f315b770c691370b6f71
AldiPradanahakim <aldi.pradan.hakim26@gmail.com>
2023-02-14 19:31:59 +0700

    mengupload tugas programing web 1

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,34 @@
+<?php
+
+$nama = 'Aldi Pradana Hakim';
+$mata_kuliah = 'Pemrograman Web';
+
+//$mata_kuliah_umum // snake_case
+//$matakuliahumum // camelcase
+//$MataKuliahUmum // pascal case
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 2</title>
+</head>
+<body>
+
+    <h1>
+        <?php echo "Hello, $nama"; ?>
+    </h1>
+    
+    <p><?php echo $mata_kuliah ; ?></p>
+    <p><?php echo 'Halo, nama saya' . $nama . ', saya sedang kuliah "' . $mata_kuliah . '"' ; ?></p>
+
+
+
+
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,30 @@
+<?php 
+
+$x = 35;
+
+$hasilkali = 5 * $x;
+$hasilbagi = $hasilkali / 2; 
+$ditambah = $hasilbagi + 75;
+$dikurang = $ditambah - 20;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1b</title>
+</head>
+<body>
+    
+    <p>Aku adalah angka <b><?php echo $x ?></b><br>
+    Jika aku dikali 5, maka aku sekarang menjadi <b><?php echo $hasilkali ?></b><br>
+    Jika aku dikali 2, maka aku sekarang menjadi <b><?php echo $hasilbagi ?></b><br>
+    Jika aku dikali 75, maka aku sekarang menjadi <b><?php echo $ditambah ?></b><br>
+    Jika aku dikali 20, maka aku sekarang menjadi <b><?php echo $dikurang ?></b><br>
+</p>
+</body>
+</html>
